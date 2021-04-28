4efde94faaa0d1bc758b9dc7689f5735e760db87
debrajdas1982 <debraj.das@iths.se>
2021-04-28 18:46:45 +0200

    Deploy with Heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.use( express.json() )
 app.use( cors() )
 app.use( express.static(staticFolder) )
-// app.use(express.static(imgFolder))
+
 
 
 
@@ -32,7 +32,7 @@ app.use('/hamsters', hamsters);
 
 
 
-// Starta servern
+
 app.listen(PORT, () => {
 	console.log('Server listening on port ' + PORT);
 })
